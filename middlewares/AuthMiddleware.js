c7879d245e58366afc5f613a87d29ff0e2dea953
lenzLLL <70536398+lenzLLL@users.noreply.github.com>
2024-03-11 04:47:01 +0100

    Update AuthMiddleware.js

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 
 export const verifyToken = (req,res,next) => {
-    if(!req.signedCookies.jwt)
+    if(!req.cookies.jwt)
     {
         return res.status(404).send("token is not found")
     }
